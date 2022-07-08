d6021e661d1ff28a77d411fa81fc13b3e429bada
Dixit Gondaliya <98507705+Dixit-529@users.noreply.github.com>
2022-07-08 17:45:49 +0530

    Add files via upload

---
+++ proforinternship/Electronics/static/js/signup.js
@@ -0,0 +1,14 @@
+
+let password = document.getElementById("Password")
+, confirm_password = document.getElementById("confirm_Password");
+
+function validatePassword(){
+if(password.value != confirm_password.value) {
+  confirm_password.setCustomValidity("Passwords Don't Match");
+} else {
+  confirm_password.setCustomValidity('');
+}
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
---
+++ proforinternship/Electronics/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import Loginview,Signupview,Homeview ,AboutUsview ,ContectUsview,viewmobile,viewlaptop, viewaccessories ,productdelete , productedit
+
+
+
+urlpatterns=[
+    path('' , Loginview , name='login'),
+    path('signup/' , Signupview , name='signup'),
+    path('home/' , Homeview , name='Home'),
+    path('about/' , AboutUsview , name='About'),
+    path('contact/' , ContectUsview , name='Contact'),
+    path('mobile/<int:xyz>/' , viewmobile , name='mobile'),
+    path('laptop/<int:xyz>/' , viewlaptop , name='laptop'),
+    path('accessories/<int:xyz>/' , viewaccessories , name='accessories'),
+    path('del/<int:abc>/' , productdelete , name='delete'),
+    path('edit/<str:catagory>/<int:abc>/' , productedit , name='Update'),
+]
