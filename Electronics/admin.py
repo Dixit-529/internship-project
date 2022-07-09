24efba74a2ae81854fb61deec2e7d237069a81ff
Dixit Gondaliya <98507705+Dixit-529@users.noreply.github.com>
2022-07-09 10:35:01 +0530

    Delete Electronics directory

--- Electronics/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Signup , Mobiles ,Laptops ,Accessories
-
-# Register your models here.
-admin.site.register(Signup)
-admin.site.register(Mobiles)
-admin.site.register(Laptops)
-admin.site.register(Accessories)
